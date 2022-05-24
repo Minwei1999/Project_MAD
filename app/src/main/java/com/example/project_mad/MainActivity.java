d3fba0e2c0002ee145e88402734239ee86268085
Foong KH <64588731+fkhong@users.noreply.github.com>
2022-05-24 23:25:15 +0800

    hi

--- app/src/main/java/com/example/project_mad/MainActivity.java
+++ app/src/main/java/com/example/project_mad/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //trial000
+        setContentView(R.layout.activity_main); //trial999
     }
 }
