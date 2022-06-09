d75597f427b436c8c1b86ea9c6ac2643411b9ff7
Diya <diyamary1576@gmail.com>
2022-06-09 17:02:43 +0800

    profile final commit

--- app/src/main/java/com/example/project_mad/Profile.java
+++ app/src/main/java/com/example/project_mad/Profile.java
@@ -4,10 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Profile extends AppCompatActivity {
     ImageButton transactionsBtn;
@@ -16,11 +21,25 @@ public class Profile extends AppCompatActivity {
     ImageButton reportBtn;
     ImageButton profileBtn;
 
+    TextInputLayout username, email, password, contact;
+    TextView emaillabel;
+    String uname, emailid, pswd, number;
+
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        reference= FirebaseDatabase.getInstance().getReference("users");
+
+        username = findViewById(R.id.uname_profile);
+        email = findViewById(R.id.email_profile);
+        password = findViewById(R.id.pswd_profile);
+        contact = findViewById(R.id.contact_profile);
+
+        showAllUserData();
 
         //Image Button links
 
@@ -54,6 +73,52 @@ public class Profile extends AppCompatActivity {
             startActivity(new Intent(this,Profile.class));
         });
         //Image button links end
+    }
+
+    private void showAllUserData(){
+        Intent intent = getIntent();
+        emailid = intent.getStringExtra("emailLogin");
+        pswd = intent.getStringExtra("passwordLogin");
+        number = intent.getStringExtra("phoneNumRegister");
+
+        email.getEditText().setText(emailid);
+        password.getEditText().setText(pswd);
+        contact.getEditText().setText(number);
+
+
+    }
+
+    public void update(View view){
+
+        if (isNameChanged() || isPasswordChanged()){
+            Toast.makeText(this, "Data has been updated!", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this, "Data has not been updated!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isNameChanged() {
+        if (!uname.equals(username.getEditText().getText().toString())){
+            reference.child(uname).child("name").setValue(username.getEditText().getText().toString());
+            uname=username.getEditText().getText().toString();
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    private boolean isPasswordChanged(){
+        if(!pswd.equals(password.getEditText().getText().toString())){
+            reference.child(emailid).child("password").setValue(password.getEditText().getText().toString());
+            pswd=password.getEditText().getText().toString();
+            return true;
+        }
+        else{
+            return false;
+        }
+
     }
 
     public void logout (View view) {
